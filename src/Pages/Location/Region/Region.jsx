5e54b94c42e89fd36f0b4ab9e5a1f4a5e69e87c9
huyhoanglc <94850159+huyhoanglc@users.noreply.github.com>
2023-12-06 00:37:19 +0700

    V26
    all

--- src/Pages/Location/Region/Region.jsx
+++ src/Pages/Location/Region/Region.jsx
@@ -27,18 +27,18 @@ function Region() {
         <div class="col-md-6 d-flex align-items-stretch">
           <div class="card" style={{backgroundImage: "url(img/Region/Sydney-05.jpg)"}} data-aos="fade-up" data-aos-delay="100">
             <div class="card-body">
-              <h5 class="card-title"><a href="">Southestern Hemisphere</a></h5>
+              <h5 class="card-title"><a href="/South">Southestern Hemisphere</a></h5>
               <p class="card-text">"The term "Southeastern Hemisphere" is not commonly used to refer to a specific geographic region. However, if you are referring to monuments in the southern part of the Eastern Hemisphere (including Southeast Asia and Oceania), here are descriptions of some notable monuments in that general area"</p>
-              <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+              <div class="read-more"><a href="/South"><i class="bi bi-arrow-right"></i> Read More</a></div>
             </div>
           </div>
         </div>
         <div class="col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
           <div class="card" style={{backgroundImage: "url(img/Region/Eiffel-03.jpg)"}} data-aos="fade-up" data-aos-delay="200">
             <div class="card-body">
-              <h5 class="card-title"><a href="">Western hemisphere</a></h5>
+              <h5 class="card-title"><a href="/West">Western hemisphere</a></h5>
               <p class="card-text">"The Western Hemisphere is home to a diverse range of monuments that reflect the cultural, historical, and architectural heritage of the Americas. Here are descriptions of some notable monuments in the Western Hemisphere"</p>
-              <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+              <div class="read-more"><a href="/West"><i class="bi bi-arrow-right"></i> Read More</a></div>
             </div>
           </div>
 
@@ -46,19 +46,19 @@ function Region() {
         <div class="col-md-6 d-flex align-items-stretch mt-5">
           <div class="card" style={{backgroundImage: "url(img/Region/Statue-02.jpg)"}} data-aos="fade-up" data-aos-delay="100">
             <div class="card-body">
-              <h5 class="card-title"><a href="">Noththern Hemisphere</a></h5>
+              <h5 class="card-title"><a href="/North">Noththern Hemisphere</a></h5>
               <p class="card-text">"
 The Northern Hemisphere is home to a diverse array of monuments that showcase the cultural, historical, and architectural richness of the region. Here are descriptions of some notable monuments in the Northern Hemisphere"</p>
-              <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+              <div class="read-more"><a href="/North"><i class="bi bi-arrow-right"></i> Read More</a></div>
             </div>
           </div>
         </div>
         <div class="col-md-6 d-flex align-items-stretch mt-5">
           <div class="card" style={{backgroundImage: "url(img/Region/Himeji-03.jpg)"}} data-aos="fade-up" data-aos-delay="200">
             <div class="card-body">
-              <h5 class="card-title"><a href="">Eastern Hemisphere</a></h5>
+              <h5 class="card-title"><a href="/East">Eastern Hemisphere</a></h5>
               <p class="card-text">"The Eastern Hemisphere is home to a multitude of iconic monuments that stand as testaments to the rich history, diverse cultures, and architectural brilliance of the region. Here are descriptions of some notable monuments in the Eastern Hemisphere"</p>
-              <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+              <div class="read-more"><a href="/East"><i class="bi bi-arrow-right"></i> Read More</a></div>
             </div>
           </div>
         </div>
@@ -66,9 +66,9 @@ The Northern Hemisphere is home to a diverse array of monuments that showcase th
       <div class="wonders">
           <div class="card"  data-aos="fade-up" data-aos-delay="100">
             <div class="card-body">
-              <h5 class="card-title"><a href="">Seven Wonders</a></h5>
+              <h5 class="card-title"><a href="/Wonder">Seven Wonders</a></h5>
               <p class="card-text">"The Seven Wonders of the Ancient World is a list of magnificent structures from ancient times, marking the marvels of architecture and art in human history"</p>
-              <div class="read-more"><a href="#"><i class="bi bi-arrow-right"></i> Read More</a></div>
+              <div class="read-more"><a href="/Wonder"><i class="bi bi-arrow-right"></i> Read More</a></div>
             </div>
           </div>
         </div>
